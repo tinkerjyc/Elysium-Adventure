807fb83165218046a436189147ea2836e375feb0
Yunxiang-Li <kitzukikokone@gmail.com>
2022-04-18 14:26:30 -0400

    Firebase image upload issue

---
+++ app/src/main/java/neu/madm/awesome/BagActivity.java
@@ -0,0 +1,66 @@
+package neu.madm.awesome;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class BagActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private ItemAdapter mAdapter;
+
+    private ProgressBar mProgressCircle;
+
+    private DatabaseReference mDatabaseRef;
+    private List<Upload> mUploads;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bag);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mProgressCircle = findViewById(R.id.progress_circle);
+
+        mUploads = new ArrayList<>();
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    mUploads.add(upload);
+                }
+
+                mAdapter = new ItemAdapter(BagActivity.this, mUploads);
+
+                mRecyclerView.setAdapter(mAdapter);
+                mProgressCircle.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(BagActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                mProgressCircle.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
--- app/src/main/java/neu/madm/awesome/MainActivity.java
+++ app/src/main/java/neu/madm/awesome/MainActivity.java
@@ -1,38 +1,102 @@
 package neu.madm.awesome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
-import edu.neu.madcourse.awesome.R;
+import android.widget.ProgressBar;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private ProgressBar mProgressBar;
+    private Uri mImageUri;
+    private ImageButton mImageButton;
+
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    private StorageTask mUploadTask;
+
+    private String currName = "default";
+    private String currDescription = "default";
+    private TextView mTextViewOpenBag;
+    private Set<String> nameSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mProgressBar = findViewById(R.id.progress_bar);
+        mTextViewOpenBag = findViewById(R.id.open_bag);
+        mTextViewOpenBag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBagActivity();
+            }
+        });
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("playerBag");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("playerBag");
     }
 
     public void knifeWithBloodImageOnClick(View v) {
+        //clickItemSelected();
+        mImageUri = Uri.parse("android.resource://neu.madm.awesome/" + R.drawable.knifewithbloodbig);
         AlertDialog.Builder alertadd = new AlertDialog.Builder(MainActivity.this);
         LayoutInflater factory = LayoutInflater.from(MainActivity.this);
         final View view = factory.inflate(R.layout.knife_with_blood, null);
         alertadd.setView(view);
-        alertadd.setPositiveButton("Cancel!", new DialogInterface.OnClickListener() {
+        alertadd.setTitle("This is a knife with blood");
+        alertadd.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dlg, int sumthin) {
 
             }
         });
-        alertadd.setNegativeButton("Put into bag!", new DialogInterface.OnClickListener() {
+        alertadd.setNegativeButton("Put into bag", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dlg, int sumthin) {
-                Toast toast=Toast.makeText(getApplicationContext(),
-                        "Current evidence is put inside the bag",Toast. LENGTH_SHORT);
-                toast.show();
+                if (mUploadTask != null && mUploadTask.isInProgress()) {
+                    Toast.makeText(MainActivity.this, "in progress", Toast.LENGTH_SHORT).show();
+                } else {
+                    currName = "knifewithblood";
+                    currDescription = "This is a knife with blood";
+                    TryPutIntoBag();
+                }
             }
         });
 
@@ -40,24 +104,108 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void deadBodyImageOnClick(View v) {
+
+        //clickItemSelected();
+        mImageUri = Uri.parse("android.resource://neu.madm.awesome/" + R.drawable.deadbodybig);
         AlertDialog.Builder alertadd = new AlertDialog.Builder(MainActivity.this);
         LayoutInflater factory = LayoutInflater.from(MainActivity.this);
         final View view = factory.inflate(R.layout.dead_body, null);
         alertadd.setView(view);
-        alertadd.setPositiveButton("Cancel!", new DialogInterface.OnClickListener() {
+        alertadd.setTitle("This is a dead body");
+        alertadd.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dlg, int sumthin) {
 
             }
         });
-        alertadd.setNegativeButton("Examine!", new DialogInterface.OnClickListener() {
+        alertadd.setNegativeButton("Examine", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dlg, int sumthin) {
-                Toast toast=Toast.makeText(getApplicationContext(),
-                        "Dead body is examined",Toast. LENGTH_SHORT);
-                toast.show();
+                if (mUploadTask != null && mUploadTask.isInProgress()) {
+                    Toast.makeText(MainActivity.this, "in progress", Toast.LENGTH_SHORT).show();
+                } else {
+                    currName = "DeadBody";
+                    currDescription = "This is a dead body";
+                    TryPutIntoBag();
+                }
             }
         });
 
         alertadd.show();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+
+            Picasso.with(this).load(mImageUri).into(mImageButton);
+        }
+    }
+
+    // Get different file extension conditions(jpg, png).
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void TryPutIntoBag()
+    {
+        if (mImageUri != null)
+        {
+            StorageReference fileReference = mStorageRef.child(currName
+                    + "." + getFileExtension(mImageUri));
+
+            mUploadTask = fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mProgressBar.setProgress(0);
+                                }
+                            }, 500);
+
+                            Toast.makeText(MainActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
+                            Upload upload = new Upload(currName,
+                                    fileReference.getDownloadUrl().toString(), currDescription);
+                            String uploadId = mDatabaseRef.push().getKey();
+                            mDatabaseRef.child(uploadId).setValue(upload);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            mProgressBar.setProgress((int) progress);
+                        }
+                    });
+        }
+        else
+        {
+            Toast.makeText(this, "No interactive component selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void openBagActivity() {
+        Intent intent = new Intent(this, BagActivity.class);
+        startActivity(intent);
+    }
+
+    private void clickItemSelected() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
 }
